dfca8d5f0157adcd69f1d539c6be683618ae528c
root <root@openkm.igssgt.org>
2022-09-21 15:09:24 -0600

    configuraciones 21092022

--- code/code-dev/resources/views/connect/master.blade.php
+++ code/code-dev/resources/views/connect/master.blade.php
@@ -9,6 +9,8 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="{{url('/static/css/connect.css?v='.time())}}">
+    <link rel="shortcut icon" href="{{ url('/img/Isotipo.ico') }}" type="image/x-icon">
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/6f5a4bc953.js" crossorigin="anonymous"></script>
